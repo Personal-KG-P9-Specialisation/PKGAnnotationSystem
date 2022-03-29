695d262b893fa6977a01e8021bc286e327fcc988
test <abiram98@gmail.com>
2022-03-29 11:24:23 +0200

    triple annotation ready

--- preprocess.py
+++ preprocess.py
@@ -52,7 +52,56 @@ class ConversationProcessor:
             f.write('\n')
         f.close()
 
-        
+class DatasetSplitter:
+    def __init__(self, src_path, loaded_data_path, n_chars):
+        self.path = src_path
+        self.ldp = loaded_data_path
+        self.n_chars = n_chars
+        self.__load_dublicate_check_lst()
+
+    def __load_dublicate_check_lst(self):
+        f = open(self.ldp, 'r')
+        self.duplicate_lst = list()
+        for idx, line in enumerate(f):
+            conv = json.loads(line)
+            self.duplicate_lst.append(conv['text'][:self.n_chars])
+        f.close()
+    def format_write_path(self,partition_no):
+        splits = self.path.split('.')
+        return splits[0]+str(partition_no)+'.'+splits[1]
+
+    def split_data(self, sample_size):
+        f = open(self.path, 'r')
+        w = open(self.format_write_path(1),'w')
+        for idx,line in enumerate(f):
+            conv = json.loads(line)
+            if conv['text'][:self.n_chars] in self.duplicate_lst:
+                continue
+            if idx % sample_size == 0:
+                w.close()
+                file_path = self.format_write_path(idx)
+                print("Switching Handle to: {}".format(file_path))
+                w = open(file_path,'w')
+            w.write(json.dumps(conv)+'\n')
+        w.close()
+
+
+def remove_duplicate(path, path_anno):
+    dup_lst = list()
+    w = open(path+'.bak','w')
+    with open(path_anno,'r') as f:
+        for idx,line in enumerate(f):
+            conv = json.loads(line)
+            dup_lst.append(conv['text'][:20])
+    path_f = open(path,'r')
+    for idx,line in enumerate(path_f):
+        c = json.loads(line)
+        if c['text'][:20] in dup_lst:
+            print("Duplicate on lines {}".format(idx))
+            continue
+        w.write(line+'\n')
+    path_f.close()
+    w.close()
 
 #combing the personachat datafiles into a single souce
 def combine_dataset(path1, path2, path3, output_path):
@@ -75,6 +124,9 @@ if __name__=="__main__":
     """for valid
     d= 'personachat/personachat_'
     combine_dataset(d+'train.jsonl',d+'valid.jsonl',d+'test.jsonl', d+'combined.jsonl')"""
-    p = 'convs' 
+    """p = 'convs' 
     c = ConversationProcessor(p)
-    c.write_convs_to_jsonl("{}/conv.jsonl".format(p))
+    c.write_convs_to_jsonl("{}/conv.jsonl".format(p))"""
+    remove_duplicate('convs/convaa.jsonl', 'annotations_data/triple1.jsonl')
+    #c = DatasetSplitter("convs/conv.jsonl","annotations_data/triple1.jsonl", 20)
+    #c.split_data(200)
