a5f181714b31f64de67a96627887053c140b16ff
test <abiram98@gmail.com>
2022-04-06 10:48:03 +0200

    ent linking data fix

---
+++ annotations_data/process.py
@@ -0,0 +1,22 @@
+import json
+
+def dublicate_check(path):
+    data_w_n = list()
+    f = open(path,'r')
+    for idx,line in enumerate(f):
+        j_line = json.loads(line)
+        data_w_n.append(j_line['text'])
+    len_data = len(data_w_n)
+
+    print("Data with Duplicate is {}\nData without Duplicate is {}".format(len_data, len(set(data_w_n))))
+def check_ids(path_anno):
+    conv_ids = []
+    with open(path_anno,'r') as f:
+        for l in f:
+            conv_id = json.loads(l)['conv_id']
+            conv_ids.append(conv_id)
+    print("Data with Duplicate is {}\nData without Duplicate is {}".format(len(conv_ids), len(set(conv_ids))))
+
+if __name__ == "__main__":
+    check_ids("temp/april5_trpl.jsonl.bak")
+
